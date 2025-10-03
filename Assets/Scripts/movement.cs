2c808d672d64a170be7820bd88d57824cdb700f4
unknown <h24emwe@axevalla.fhsk.se>
2025-10-03 11:18:31 +0200

    Lamp

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -11,9 +11,8 @@ public class movement : MonoBehaviour
     public float playerspeed = 1f;
     public float jumpspeed = 1f;
     Rigidbody2D m_Rigidbody;
-    public float m_Thrust = 20f;
     
-    public float targetTime = 60.0f;
+  
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,8 +23,13 @@ public class movement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { targetTime -= Time.deltaTime;
-        
+    { 
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_Rigidbody.AddForce(Vector3.up * jumpspeed);
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * playerspeed);
@@ -34,17 +38,7 @@ public class movement : MonoBehaviour
         {
             transform.Translate(Vector3.right * playerspeed);
         }
-        if (targetTime <= 0.0f)
-        {
-            timerEnded();
+        
         }
     }
 
-    void timerEnded()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            m_Rigidbody.AddForce(Vector3.up * jumpspeed);
-        }
-    }
-}
