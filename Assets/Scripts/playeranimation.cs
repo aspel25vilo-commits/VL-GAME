66793c774def5a2260385f8335c659c8e8f2ca32
unknown <h24emwe@axevalla.fhsk.se>
2025-10-15 09:29:26 +0200

    animtion

---
+++ Assets/Scripts/playeranimation.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class playeranimation : MonoBehaviour
+{
+    private Animator objAnimator;
+    private SpriteRenderer fhillip;
+    private void Start()
+    {
+        objAnimator = GetComponent<Animator>();
+        fhillip = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D)){
+            fhillip.flipX = false;
+            objAnimator.SetBool("runleft", true);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            fhillip.flipX = true;
+            objAnimator.SetBool("runleft", true);
+        }
+        else
+        {
+            objAnimator.SetBool("runleft", false);
+        }
+    }
+
+}
+
